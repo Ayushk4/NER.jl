bbba836a15df7bb0d20ec5974618fe4d1ff1dc7a
Ayushk4 <ayushk4@gmail.com>
2019-07-02 20:20:43 +0530

    Char Embedding

--- bilstm-cnn.jl
+++ bilstm-cnn.jl
@@ -3,6 +3,8 @@ using MultiResolutionIterators
 using TextAnalysis
 using WordTokenizers
 using Embeddings
+using Flux
+using Flux: onehot, Conv
 
 train = load(CoNLL(), "train") # training set
 # test = load(CoNLL(), "test") # test set
@@ -15,17 +17,39 @@ typeof(dataset)
 X_train = [CorpusLoaders.word.(sent) for sent in dataset]
 Y_train = [CorpusLoaders.named_entity.(sent) for sent in dataset]
 
-# Preprocessing: Change n't => not
+# Preprocessing: Change n't => not and Shuffle
 
 # X = X_train[1:1000]
 # tag_scheme!(tags, "BIO2". "BIOES")
 
 words_vocab = unique(vcat(X_train...))
-alphabet = sort(unique(vcat(collect.(words_vocab)...)))
-push!(pad and unk)
+alphabet = [:unksort(unique(vcat(collect.(words_vocab)...)))]
+typeof(alphabet)
+
+UNK = '¿'
+PAD = 'ϕ'
+@assert UNK ∉ alphabet && PAD ∉ alphabet
+push!(alphabet, UNK)
+push!(alphabet, PAD) # PAD is required for CNNs while generating embeddings.
+sort!(unique!(alphabet))
 
 # 1. Character Embeddings - Take care of unknown, add padding.
-# 2. Word Embeddings.
+# 2. Word Embeddings. - Take care of unknown
 # 3. hcat Character and Word Embeddings
 # 4. Bi-LSTM
-# 5. Softmax
+# 5. Softmax or CRF
+
+CHAR_EMBED_DIMS = 25
+CNN_OUTPUT_SIZE = 53
+
+CONV_WINDOW_LENGTH = 3
+LSTM_STATE_SIZE = 253
+DROPOUT_CNN = 0.68
+
+# 1. Dropout before conv, Max poll layer, PADDING on both sides, hyperparams = window size, output vector size.
+
+W_Char_Embed = rand(CHAR_EMBED_DIMS, length(alphabet)) .- 0.5 # Bringing into range b/w -0.5 and 0.5
+PADDING
+Chain(Dropout(DROPOUT_CNN), Conv((CHAR_EMBED_DIMS, CONV_WINDOW_LENGTH), 1=>CNN_OUTPUT_SIZE))
+
+# function test_raw_statement # Convert unknown to UNK and to lowercase
