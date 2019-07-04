23fa369fdbac8ca0e104a9c3b384402339bf36ba
Ayushk4 <ayushk4@gmail.com>
2019-07-05 00:34:13 +0530

    Debug Char embedding

--- bilstm-cnn.jl
+++ bilstm-cnn.jl
@@ -23,7 +23,7 @@ Y_train = [CorpusLoaders.named_entity.(sent) for sent in dataset]
 # tag_scheme!(tags, "BIO2". "BIOES")
 
 words_vocab = unique(vcat(X_train...))
-alphabet = [:unksort(unique(vcat(collect.(words_vocab)...)))]
+alphabet = [:unk, sort(unique(vcat(collect.(words_vocab)...)))]
 typeof(alphabet)
 
 UNK = '¿'
@@ -49,7 +49,11 @@ DROPOUT_CNN = 0.68
 # 1. Dropout before conv, Max poll layer, PADDING on both sides, hyperparams = window size, output vector size.
 
 W_Char_Embed = rand(CHAR_EMBED_DIMS, length(alphabet)) .- 0.5 # Bringing into range b/w -0.5 and 0.5
-PADDING
-Chain(Dropout(DROPOUT_CNN), Conv((CHAR_EMBED_DIMS, CONV_WINDOW_LENGTH), 1=>CNN_OUTPUT_SIZE))
 
-# function test_raw_statement # Convert unknown to UNK and to lowercase
+char_representation = Chain(x -> reshape(x, size(x)..., 1,1),
+      Dropout(DROPOUT_CNN),
+      Conv((CHAR_EMBED_DIMS, CONV_WINDOW_LENGTH), 1=>CNN_OUTPUT_SIZE)
+      x -> maximum(x, dims=2)
+      )
+
+# function test_raw_sentence # Convert unknown to UNK and to lowercase
