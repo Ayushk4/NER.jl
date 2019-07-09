65fda6ac0f2412b3abd7ed75e63a91939365fef9
Ayushk4 <ayushk4@gmail.com>
2019-07-09 16:47:37 +0530

    Handle unk, and switch to onehot for embedding

--- bilstm-cnn.jl
+++ bilstm-cnn.jl
@@ -6,6 +6,9 @@ using Embeddings
 using Flux
 using Flux: onehot, Conv, param
 
+CHAR_EMBED_DIMS = 25
+WORD_EMBED_DIMS = 50
+
 train = load(CoNLL(), "train") # training set
 # test = load(CoNLL(), "test") # test set
 # dev = load(CoNLL(), "dev") # dev set
@@ -17,69 +20,110 @@ typeof(dataset)
 X_train = [CorpusLoaders.word.(sent) for sent in dataset]
 Y_train = [CorpusLoaders.named_entity.(sent) for sent in dataset]
 
-# TODO: Preprocessing: Change n't => not and Shuffle and all numbers to 0
-
-# X = X_train[1:1000]
-# tag_scheme!(tags, "BIO2". "BIOES")
+@assert length.(X_train) == length.(Y_train)
+# tag_scheme!.(Y_train, "BIO2". "BIOES")
 
 words_vocab = unique(vcat(X_train...))
+alphabets = unique(vcat(collect.(words_vocab)...))
+labels = unique(vcat(Y_train...))
 
-UNK = '¿'
-alphabet = unique(vcat(collect.(words_vocab)...))
-@assert UNK ∉ alphabet
-push!(alphabet, UNK)
-sort!(unique!(alphabet))
+# TODO: Preprocessing: Change n't => not and Shuffle and all numbers to 0
 
-onehot(, vocab) = [onehot(word, vocab) for word in sentence]
 
-batches(xs,)
 
-# Char Embeddings, using these indices to generate the flux one hot input word embeddings vectors.
+#### Word Embeddings
+# using these indices to generate the flux one hot input word embeddings vectors.
 
-W_Char_Embed = rand(CHAR_EMBED_DIMS, length(alphabet)) .- 0.5 # Bringing into range b/w -0.5 and 0.5
-W_Char_Embed = param(W_Char_Embed)
-
-# Word Embeddings, using these indices to generate the flux one hot input word embeddings vectors.
-
+# TODO: EMBEDDINGS OF the input size.
 embtable = load_embeddings(GloVe)
-get_word_index = Dict(word=>ii for (ii,word) in enumerate(embtable.vocab))
+get_word_index = Dict(word => ii for (ii, word) in enumerate(embtable.vocab))
 get_word_from_index = Dict(value => key for (key, value) in get_word_index)
+W_word_Embed = embtable.embeddings
 
+# One Vec for unknown chars
+UNK_Word = "<UNK>"
+UNK_Word_Idx = length(get_word_index) + 1
+embedding_vocab_length = length(get_word_index) + 1
+@assert UNK_Word ∉ collect(keys(get_word_index))
+
+get_word_index[UNK_Word] = UNK_Word_Idx
+get_word_from_index[UNK_Word_Idx] = UNK_Word
+W_word_Embed = hcat(W_word_Embed, rand(WORD_EMBED_DIMS))
+@assert size(W_word_Embed, 2) == embedding_vocab_length
+
+
+
+##### Char Embeddings
+UNK_char = '¿'
+@assert UNK_char ∉ alphabets
+push!(alphabets, UNK_char)
+sort!(unique!(alphabets))
+
+# using these indices to generate the flux one hot input Char embeddings vectors.
+
+W_Char_Embed = rand(CHAR_EMBED_DIMS, length(alphabets)) .- 0.5 # Bringing into range b/w -0.5 and 0.5
+W_Char_Embed = param(W_Char_Embed)
+get_char_index = Dict(char => ii for (ii, char) in enumerate(alphabets))
+get_char_from_index = Dict(value => key for (key, value) in get_char_index)
+
+
+############# Creating onehoot vectors (useful for embeddings lookup)
+# TODO: Minibatches
+onehotword(word) = onehot(get(get_word_index, lowercase(word), get_word_index[UNK_Word]), 1:embedding_vocab_length)
+onehotchars(word) = [onehot(get(get_char_index, c, get_char_index[UNK_char]), 1:length(alphabets)) for c in word]
+onehotlabel(label) = onehot(label, labels)
+
+oh_seq(arr, f) = [f(element) for element in arr]
+
+X_words_train = [oh_seq(sentence, onehotword) for sentence in X_train] # A Bunch of Sequences of words, i.e. sentences
+
+X_chars_train = [oh_seq(sentence, onehotchars) for sentence in X_train] # A Bunch Sequences of Array of Chars, done to prevent repeated computations.
+
+Y_oh_train = [oh_seq(tags_sequence, onehotlabel) for tags_sequence in Y_train]
+
+@assert length.(X_train) == length.(X_words_train) ==
+        length.(X_chars_train) == length.(Y_oh_train)
+
+#################### MODEL ######################
 # 1. Character Embeddings - Take care of unknown, add padding.
 # 2. Word Embeddings. - Take care of unknown
 # 3. hcat Character and Word Embeddings
 # 4. Bi-LSTM
 # 5. Softmax or CRF
 
-CHAR_EMBED_DIMS = 25
 CNN_OUTPUT_SIZE = 53
-
 CONV_WINDOW_LENGTH = 3
 LSTM_STATE_SIZE = 253
 DROPOUT_CNN = 0.68
 
 # 1. Dropout before conv, Max poll layer, PADDING on both sides, hyperparams = window size, output vector size.
 
-char_features = Chain(x -> reshape(x, size(x)..., 1,1),
-      Dropout(DROPOUT_CNN),
-      Conv((CHAR_EMBED_DIMS, CONV_WINDOW_LENGTH), 1=>CNN_OUTPUT_SIZE, pad=(0,2)),
-      x -> maximum(x, dims=2)
-      )
+char_features = Chain(cs -> reshape(cs, size(x)..., 1,1),
+                      Dropout(DROPOUT_CNN),
+                      Conv((CHAR_EMBED_DIMS, CONV_WINDOW_LENGTH), 1=>CNN_OUTPUT_SIZE, pad=(0,2)),
+                      x -> maximum(x, dims=2)
+                     )
 
 # 2. Word Embeddings
 
 # Maybe could use only the embeddings for the words needed
-W_word_Embed = embtable.embeddings
-# W_word_Embed = param(embtable.embeddings)
+
+# W_word_Embed = param(W_word_Embed)
 
 get_word_embedding(word) = W_word_Embed[:, get_word_index[word]]
+Change above to onehot
 
 # 3. Final input Embeddings
 
 input_embeddings(w, cs) = hcat(get_word_embedding(w), dropdims(char_features(cs)))
 
 # 4. Bi-LSTM
-
+bilstm_layer
 # TODO: Bias vectors are initialized to zero, except the bias bf for the forget gate in LSTM , which is initialized to 1.0
-m(x, y) = Chain()
+
+# 5. Softmax
+
+m(x,y) = softmax(blstm_layer(x,y))
+
 # function test_raw_sentence # Convert unknown to UNK and to lowercase
+# TODO: Test with and without lowercased chars in char embedding
