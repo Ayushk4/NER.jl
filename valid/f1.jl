904c027929eaf718950427a18d2ecbc5d67d5805
Ayushk4 <ayushk4@gmail.com>
2019-08-06 19:31:46 +0530

    Valid for CoNLL

--- valid/f1.jl
+++ valid/f1.jl
@@ -1,25 +1,27 @@
 using TextAnalysis, Flux
-include("../src/ner.jl")
+using LinearAlgebra
+# include("../src/ner.jl")
 
-function try_outs(ner::NERmodel, x_in, y_in)
+function try_outs(ner, x_in, y_in)
     unique_labels = unique(ner.model.labels)
     num_labels = length(unique_labels)
     confusion_matrix = zeros(Int, (num_labels, num_labels))
 
-    for (x_seq, y_seq) in zip(X_input_dev, Y_oh_dev)
+    for (x_seq, y_seq) in zip(x_in, y_in)
         preds = ner(x_seq)
 
         for (pred, logit) in zip(preds, y_seq)
-            confusion_matrix[findfirst(pred, unique_labels), findfirst(logit, unique_labels)] += 1
+            confusion_matrix[findfirst(x -> x==pred, unique_labels), findfirst(x -> x==logit, unique_labels)] += 1
         end
     end
 
+    # print(confusion_matrix)
     s1 = sum(confusion_matrix, dims=2)
     s2 = sum(confusion_matrix, dims=1)
     dg = diag(confusion_matrix)
 
-    a = sum(dg ./ s1) / 5
-    b = sum(dg ./ s2) / 5
+    a = sum(dg ./ s1) / num_labels
+    b = sum(dg ./ s2') / num_labels
 
     println("Precision and recall are:", a, " ", b)
     println("F1 is:", (2 * a * b) / (a + b))
